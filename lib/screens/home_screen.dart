1574dde36c0941fe3ff5f470a00fb43bdf28fa54
Lans12345 <olanalans12345@gmail.com>
2024-09-23 15:35:34 +0800

    added functionality on announcement

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -40,6 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: primary,
         title: TextWidget(
           text: labels[_selectedIndex],
